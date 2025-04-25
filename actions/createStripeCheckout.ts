ec57b29649ce710a0fe1197f4cd0ad200b5d8f46
BensonRaro <bensonraro22@gmail.com>
2025-04-25 18:10:29 +0300

    update

--- actions/createStripeCheckout.ts
+++ actions/createStripeCheckout.ts
@@ -17,7 +17,7 @@ export async function createStripeCheckout(plan: string, user: userProps) {
     if (!user) {
       throw new Error("User not found");
     }
-
+    const priceInCents = Math.round(10 * 100);
     // 3. Create and configure Stripe Checkout Session with course details
     const session = await stripe.checkout.sessions.create({
       line_items: [
@@ -26,16 +26,17 @@ export async function createStripeCheckout(plan: string, user: userProps) {
             currency: "usd",
             product_data: {
               name: plan,
+              description: plan,
               images: ["/favicon.ico"],
             },
-            unit_amount: 1000, // $10.00
+            unit_amount: priceInCents, // $10.00
           },
           quantity: 1,
         },
       ],
       mode: "payment",
-      success_url: `${baseUrl}/dashboard?success=true`,
-      cancel_url: `${baseUrl}/dashboard?success=false`,
+      success_url: `https://canvaclonebr.vercel.app/dashboard?success=true`,
+      cancel_url: `https://canvaclonebr.vercel.app/dashboard?success=false`,
       metadata: {
         plan: plan,
         userId: user?.data?._id ?? "",
