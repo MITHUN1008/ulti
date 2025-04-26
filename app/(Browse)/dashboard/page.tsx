d9e8600b9323fbcef2e7d313edf1f457f364b168
BensonRaro <bensonraro22@gmail.com>
2025-04-26 23:58:34 +0300

    update

--- app/(Browse)/dashboard/page.tsx
+++ app/(Browse)/dashboard/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import Banner from "@/components/dashboard/Banner";
 import RecentDesigns from "@/components/dashboard/RecentDesigns";
 import Sizes from "@/components/dashboard/Sizes";
 import Offline from "@/components/global/Offline";
+import { runFireworks } from "@/lib/Confetti";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const DashboardPage = () => {
+  const searchParams = useSearchParams();
+  const success = searchParams.get("success") || "false";
+
+  useEffect(() => {
+    if (success === "true") {
+      runFireworks();
+    }
+  }, []);
   return (
     <div className="flex flex-col space-y-6 p-10 mx-auto py-10">
       <Banner />
--- app/(Browse)/page.tsx
+++ app/(Browse)/page.tsx
@@ -1,26 +1,10 @@
-"use client";
-
-import ReactConfetti from "@/components/dashboard/ReactConfetti";
 import Templates from "@/components/design/sidebar/Templates";
 import Hero from "@/components/home/Hero";
 import Pricing from "@/components/home/Pricing";
-import { runFireworks } from "@/lib/Confetti";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const success = searchParams.get("success") || "false";
-
-  useEffect(() => {
-    if (success === "true") {
-      runFireworks();
-    }
-  }, []);
-
   return (
     <div className="space-y-20 mb-4 mt-20">
-      <ReactConfetti />
       <Hero />
       <Pricing />
       <div className="mx-10 lg:mx-20">
